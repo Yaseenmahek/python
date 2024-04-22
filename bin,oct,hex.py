d33dcd7e8bad8a538d3b63b40de35bbbb05d0d2f
Yaseenmahek <167419587+Yaseenmahek@users.noreply.github.com>
2024-04-22 16:52:18 +0530

    Add files via upload

---
+++ bin,oct,hex.py
@@ -0,0 +1,30 @@
+
+print('oct(10) is:', oct(10))
+
+
+print('oct(0b101) is:', oct(0b101))
+
+
+print('oct(0XA) is:', oct(0XA))
+
+number = 15
+
+print('The binary equivalent of 15 is', bin(number))
+
+
+
+number = 5 
+
+print('The binary equivalent of 5 is:', bin(number))
+
+number = 435
+print(number, 'in hex =', hex(number))
+
+number = 0
+print(number, 'in hex =', hex(number))
+
+number = -34
+print(number, 'in hex =', hex(number))
+
+returnType = type(hex(number))
+print('Return type from hex() is', returnType)
